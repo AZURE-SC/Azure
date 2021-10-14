ab2764b5808e19881d08d2b6eada805a69476b23
neysertg <neysser_10@hotmail.com>
2021-10-14 16:05:24 -0500

    Agregando L vista de Usuuario(Alumno) con BD

--- Models/Products.cs
+++ Models/Products.cs
@@ -8,8 +8,12 @@ namespace AMARILLO.Models
     public class Products
     {
         public string id { get; set; }
-        public string Name { get; set; }
-        public string Cost { get; set; }
-        public string QTY { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Curso { get; set; }
+        
+
+        
     }
 }
